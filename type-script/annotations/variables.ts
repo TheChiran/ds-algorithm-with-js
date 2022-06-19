6c733104a8855a7d8701c67d4907c0b092b998fe
TheChiran <tonmoychiran449@gmail.com>
2022-06-19 20:47:55 +0600

    [Task-> 1. did some practice on typescript]

---
+++ type-script/annotations/variables.ts
@@ -0,0 +1,33 @@
+import { CaaRecord } from "dns";
+
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red','green','blue'];
+let numbers: number[] = [1,2,3,4,5];
+
+// classes
+class Car{
+
+}
+let car: Car = new Car();
+
+interface Point{
+  x: number;
+  y: number;
+}
+// Object literal
+let point: {x: number;y: number;}={
+  x: 10,
+  y: 20
+}
+
+// Function
+const logNumber = (i: number)=>{
+  console.log(i)
+}
